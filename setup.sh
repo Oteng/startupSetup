f4ffc216437eb5252286b68794d3e55400ed3237
Oteng <otengkwaku@gmail.com>
2013-07-19 20:58:20 +0000

    correct bug with .bashrc_custom
    correct bug with .bashrc_coustom

--- setup.sh
+++ setup.sh
@@ -11,18 +11,18 @@ wget raw.github.com/startup-class/dotfiles/master/.screenrc -O .screenrc
 git clone https://github.com/startup-class/setup.git
 ./setup/setup.sh
 
-echo "installing heroku"
- wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
- 
- echo "logging in to heroku"
- heroku login
- 
- echo "generating rsa key for heroku"
- ssh-keygen -t rsa
- heroku keys:add
- 
- echo "PS1='\[\033[01;35m\][\u@\h\[\033[00m\]:\[\033[01;33m\]\w\[\033[01;35m\]]\[\033[01;34m\]$\[\033[00m\]'" >> .bashrc_custom
- 
- echo "exiting pls log back in to see changes"
- exit
+echo "installing heroku\n"
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+echo "logging in to heroku\n"
+heroku login
+
+echo "generating rsa key for heroku"
+ssh-keygen -t rsa
+heroku keys:add
+
+echo "PS1='\[\033[01;35m\][\u@\h\[\033[00m\]:\[\033[01;33m\]\w\[\033[01;35m\]]\[\033[01;34m\]$\[\033[00m\]'" >> ~/.bashrc_custom
+
+echo "exiting pls log back in to see changes"
+exit
  
