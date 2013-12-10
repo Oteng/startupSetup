c90d14d66a0baf74530a399cded1db490beca0d3
Oteng <otengkwaku@gmail.com>
2013-12-10 04:48:23 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,18 +11,28 @@ wget raw.github.com/startup-class/dotfiles/master/.screenrc -O .screenrc
 git clone https://github.com/startup-class/setup.git
 ./setup/setup.sh
 
+echo "\n\n[\033[01;30m\]"
 echo "installing heroku\n"
+echo "\n\n[\033[00m\]"
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
+echo "\n\n[\033[01;30m\]"
 echo "logging in to heroku\n"
+echo "\n\n[\033[00m\]"
+
 heroku login
 
+echo "\n\n[\033[01;30m\]"
 echo "generating rsa key for heroku"
+echo "\n\n[\033[00m\]"
+
 ssh-keygen -t rsa
 heroku keys:add
 
 echo "PS1='\[\033[01;35m\][\u@\h\[\033[00m\]:\[\033[01;33m\]\w\[\033[01;35m\]]\[\033[01;34m\]$\[\033[00m\]'" >> ~/.bashrc_custom
 
+echo "\n\n[\033[01;30m\]"
 echo "exiting pls log back in to see changes"
+echo "\n\n[\033[00m\]"
 exit
  
